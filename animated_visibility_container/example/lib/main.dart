62993c0cd5fe425699a744005a34c60fd41f5c86
axilesoft <axile.app.support@outlook.com>
2019-05-28 14:21:49 +0800

    more sample code

--- animated_visibility_container/example/lib/main.dart
+++ animated_visibility_container/example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:animated_visibility_container/animated_visibility_container.dart';
+import 'package:animated_visibility_container/animated_visibility_container.dart' as avc;
 
 void main() => runApp(MyApp());
 
@@ -43,8 +43,76 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       _visible = !_visible;
+      if (_visible)
+        avcid++;
     });
   }
+
+  List<avc.AnimatedChildBuilder> builders=[
+        (BuildContext context, Animation ani) {
+      return Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateX(-pi / 2 * (1 - ani.value)),
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.indigoAccent,
+            width: 300,
+            height: 300,
+            child: Text('Sample of AnimatedVisibilityContainer',
+                style: Theme.of(context)
+                    .textTheme
+                    .title
+                    .copyWith(color: Colors.amberAccent),
+                textAlign: TextAlign.center)),
+      );
+    },
+
+        (BuildContext context, Animation ani) {
+      return Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateY(-pi / 2 * (1 - ani.value)),
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.indigoAccent,
+            width: 300,
+            height: 300,
+            child: Text('Sample of AnimatedVisibilityContainer',
+                style: Theme.of(context)
+                    .textTheme
+                    .title
+                    .copyWith(color: Colors.amberAccent),
+                textAlign: TextAlign.center)),
+      );
+    },
+        (BuildContext context, Animation ani) {
+      return Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.identity()
+          ..rotateZ(-pi * 2 * (1 - ani.value))
+        ..scale(ani.value),
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.indigoAccent,
+
+            width: 300,
+            height: 300,
+            child: Text('Sample of AnimatedVisibilityContainer',
+                style: Theme.of(context)
+                    .textTheme
+                    .title
+                    .copyWith(color: Colors.amberAccent,shadows: [Shadow(offset: Offset(0,3),blurRadius: 3)]),
+                textAlign: TextAlign.center)),
+      );
+    },
+
+
+  ];
+
+  static int avcid=0;
 
   @override
   Widget build(BuildContext context) {
@@ -53,33 +121,15 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: AnimatedVisibilityContainer(
+        child: avc.AnimatedVisibilityContainer(
             visible: _visible,
-            aniDurMs: 1000,
-            aniChildBuilder: (BuildContext context, Animation ani) {
-              return Transform(
-                alignment: Alignment.bottomCenter,
-                transform: Matrix4.identity()
-                  ..setEntry(3, 2, 0.001)
-                  ..rotateX(-pi / 2 * (1 - ani.value)),
-                child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.indigoAccent,
-                    width: 300,
-                    height: 300,
-                    child: Text('Sample of AnimatedVisibilityContainer',
-                        style: Theme.of(context)
-                            .textTheme
-                            .title
-                            .copyWith(color: Colors.amberAccent),
-                        textAlign: TextAlign.center)),
-              );
-            }),
+            aniDurMs: 300,
+            aniChildBuilder: builders[(avcid)%builders.length]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.touch_app),
       ),
     );
   }
